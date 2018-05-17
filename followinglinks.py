76887dab990b39e398c4d31670ca3989b8d732d7
mcarre93 <31905673+mcarre93@users.noreply.github.com>
2018-05-17 11:32:16 -0400

    Added followinglinks.py

---
+++ followinglinks.py
@@ -0,0 +1,28 @@
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+count = int(input('Enter count: '))
+position = int(input('Enter position: '))
+
+# Iterate (count) times
+for i in range(0, count + 1):
+    print("Retrieving: ", url)
+    pos_html = urllib.request.urlopen(url, context=ctx).read()
+    pos_soup = BeautifulSoup(pos_html, 'html.parser')
+    pos_tags = pos_soup('a')
+    # Set new position link
+    url = pos_tags[position - 1].get("href", None)
